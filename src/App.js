33c4806676b9d46cf80ad74f31b43303147f562e
LukeJessop <jessop.lucas@gmail.com>
2023-06-13 21:03:58 -0600

    pushing changes for today

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import Hero from './components/Hero/Hero';
-import Halftone from './assets/halftone.png'
-import About from './components/About/About';
-import Projects from './components/Projects/Projects';
-import Footer from './components/Footer/Footer';
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import Halftone from "./assets/halftone.png";
+import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="app">
-      <div className='main-content'>
-        <Navbar/>
-        <Hero/>
-        <About/>
-        <Projects/>
-        <Footer/>
+      <div className="main-content">
+        <Navbar />
+        <Hero />
+        <About />
+        <Projects />
       </div>
-      <div className='halftone-wrapper'>
-        <div className=' halftone-container'>
-          <img alt='halftone' className='first-halftone' src={Halftone}/>
-          <img alt='halftone' className='second-halftone' src={Halftone}/>
+      <Footer />
+      <div className="halftone-wrapper">
+        <div className=" halftone-container">
+          <img alt="halftone" className="first-halftone" src={Halftone} />
+          <img alt="halftone" className="second-halftone" src={Halftone} />
         </div>
       </div>
     </div>
